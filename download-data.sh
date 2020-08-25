43bdc43bec86928df472ad84291bb14d74efbe3d
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2020-08-25 08:27:43 -0700

    Remove data after unzip'ing and un'taring - reduces size to 60Gb.

--- download-data.sh
+++ download-data.sh
@@ -22,6 +22,7 @@ mv audio_dataset.zip data && pushd data
 mkdir audio_dataset && mv audio_dataset.zip audio_dataset && pushd audio_dataset
 unzip audio_dataset.zip && mv audio_dataset.zip ..
 popd
+rm -rf audio_dataset.zip
 popd
 curl -L "https://www.dropbox.com/s/r7sbwckqvqy7uil/TalkingMachinesPodcast.wav?dl=0" -o data/TalkingMachinesPodcast.wav
 echo "...done"
@@ -29,7 +30,7 @@ echo "...done"
 echo "Downloading Ch8 data..."
 curl -L "https://www.dropbox.com/s/39l7unjv11hvouv/User%20Identification%20From%20Walking%20Activity.zip?dl=0" -o "data/User Identification From Walking Activity.zip"
 pushd data
-unzip User*.zip &&  rm -rf __MACOSX/
+unzip User*.zip &&  rm -rf __MACOSX/ && rm -rf User*.zip
 popd
 echo "...done"
 
@@ -40,25 +41,26 @@ echo  "...done"
 echo "Downloading Ch 12 and Ch14 and Ch15 (CIFAR-10) data..."
 curl -L "https://www.dropbox.com/s/gn965hqdxrytyq3/cifar-10-python.tar.gz?dl=0" -o data/cifar-10-python.tar.gz
 pushd data
-tar xvzf cifar-10-python.tar.gz
+tar xvzf cifar-10-python.tar.gz && rm -rf cifar-10-python.tar.gz
 popd
 echo "...done"
 
 echo "Downloading Ch15 data (VGG Face)..."
 curl -L "https://www.dropbox.com/s/yq5med963kfl23w/vgg_face_dataset.tar.gz?dl=0" -o data/vgg_face_dataset.tar.gz
 pushd data
-tar xvf vgg_face_dataset.tar.gz
+tar xvf vgg_face_dataset.tar.gz && rm -rf vgg_face_dataset.tar.gz
 popd
 mkdir -p data/vgg_face
 curl -L "https://www.dropbox.com/s/xk70h7w35fm8we8/vgg-face-small.tar.gz?dl=0" -o data/vgg_face/vgg-face-small.tar.gz
 pushd data/vgg_face
 tar xvzf vgg-face-small.tar.gz && mv vgg-face-small.tar.gz ..
 popd
+rm -rf vgg-face-small.tar.gz
 curl -L "https://www.dropbox.com/s/yiacsezcmjhfn9c/vgg_face_full_urls.csv?dl=0" -o data/vgg_face_full_urls.csv
 curl -L "https://www.dropbox.com/s/tpg3aekqixhzhew/vgg_face_full.csv?dl=0" -o  data/vgg_face_full.csv
 curl -L "https://www.dropbox.com/s/jn6jcu26tx13fzz/vgg-models.zip?dl=0" -o data/vgg-models.zip
 pushd data
-unzip vgg-models.zip
+unzip vgg-models.zip && rm -rf vgg-models.zip
 popd
 curl -L "https://www.dropbox.com/s/k21cpcl43gmpfzv/vgg_face_weights.h5?dl=0" -o models/vgg_face_weights.h5
 echo "...done"
@@ -69,14 +71,13 @@ echo "...done"
 
 echo "Download Ch17 data..."
 curl -L "https://www.dropbox.com/s/bjxl20toeqqrz45/LibriSpeech.tar.gz?dl=0" -o LibriSpeech.tar.gz
-tar xvzf LibriSpeech.tar.gz
-mv LibriSpeech.tar.gz data
+tar xvzf LibriSpeech.tar.gz && rm -rf LibriSpeech.tar.gz
 echo "...done"
 
 echo "Downloading Ch18 data..."
 curl -L "https://www.dropbox.com/s/9mwr58i9fsxgexd/seq2seq.tar.gz?dl=0" -o  data/seq2seq.tar.gz
 pushd data
-tar xvzf seq2seq.tar.gz
+tar xvzf seq2seq.tar.gz && rm -rf seq2seq.tar.gz
 popd
 echo "...done"
 
@@ -84,6 +85,6 @@ echo "Downloading Ch19 data..."
 mkdir -p data/cloth_folding_rgb_vids
 curl -L "https://www.dropbox.com/sh/jjozbrynieo7hf2/AAB3mHI5DcWFIl9kEmbNPIFXa?dl=0" -o data/cloth_folding_rgb_vids/cloth_folding_vids.zip
 pushd data/cloth_folding_rgb_vids
-unzip cloth_folding_vids.zip
+unzip cloth_folding_vids.zip && rm -rf cloth_folding_vids.zip
 popd
 
