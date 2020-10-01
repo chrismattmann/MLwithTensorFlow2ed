3add31d5721f5e236a5765e55919797714a069e5
Philip Southam <philip.southam@jpl.nasa.gov>
2020-10-01 09:45:49 -0700

    Fixing face test urls in `Listing 15.14 - 15.23.ipynb` in both TFv1 and TFv2.

--- run_TFv2_environment.sh
+++ run_TFv2_environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # TODO: restrict gpu run to 127.0.0.1 again
 if [[ "$(uname)" == "Linux" && -c /dev/nvidia0 ]]; then
-    docker run --gpus all -p 8888:8888 -p 6006:6006 --rm chrismattmann/mltf2:tf2
+    docker run --gpus all -p 127.0.0.1:8888:8888 --rm chrismattmann/mltf2:tf2
 else
     docker run -p 127.0.0.1:8888:8888 --rm chrismattmann/mltf2:tf2
 fi
